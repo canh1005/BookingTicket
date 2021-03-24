9d8fe317599f201144c87343f756152a08384329
nguyenhuucanh1505@gmail.com <nguyenhuucanh1505@gmail.com>
2021-03-24 20:18:20 +0700

    Update Admin

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,12 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Route } from 'react-router'
 import "../../components/DashboardAdmin/Dashboard"
 import QuanLy from '../../components/QuanLy/QuanLy'
-export default class Admin extends Component {
-    render() {
-        return (
-            <div>
-                <QuanLy/>
-            </div>
-        )
-    }
+export default function Admin({ Component, ...props }) {
+    return (
+        <div>
+            <Route
+                {...props}
+                render={(propsComponent) => (<QuanLy>
+                    <Component {...propsComponent} />
+                </QuanLy>)}
+            />
+        </div>
+    )
 }
+
