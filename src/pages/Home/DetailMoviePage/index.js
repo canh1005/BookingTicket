620fc2c34c8d85ee622f9201afae413c74e54488
nguyenhuucanh1505@gmail.com <nguyenhuucanh1505@gmail.com>
2021-03-24 19:41:26 +0700

    Update DetailMovie

--- src/pages/Home/DetailMoviePage/index.js
+++ src/pages/Home/DetailMoviePage/index.js
@@ -3,16 +3,17 @@ import { connect } from 'react-redux'
 import * as CinemaAction from './../../../redux/modules/ListCinemaReducer/action'
 import * as DetailMovieAction from './../../../redux/modules/DetailMovieReducer/action'
 import { Grid, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography, Button } from '@material-ui/core'
+import {Link} from 'react-router-dom'
 
 function DetailMoviePage(props) {
     useEffect(() => {
-        console.log("show ID: ", props.match);
+        // console.log("show ID: ", props.match);
 
         // console.log("Data: ", props.cinemaData);
         let id = props.match.params.id;
         props.fetchDetailMovie(id);
         props.fetchListCinema();
-        console.log("Data: ", props.detailMovieData);
+        // console.log("Data: ", props.detailMovieData);
         // eslint-disable-next-line react-hooks/exhaustive-deps  
     }, [])
     const showDetailMovie = () => {
@@ -32,9 +33,9 @@ function DetailMoviePage(props) {
     const renderTable = () => {
         const { detailMovieData } = props;
         if(detailMovieData){
-            console.log("AA:1", detailMovieData.heThongRapChieu);
+            // console.log("AA:1", detailMovieData.heThongRapChieu);
             return detailMovieData.heThongRapChieu.map(item=>{
-                console.log("AA:2", item.cumRapChieu);
+                // console.log("AA:2", item.cumRapChieu);
                 return item.cumRapChieu.map(item=>{
                     return item.lichChieuPhim.map(item=>{
                         let ngayChieugioChieu = item.ngayChieuGioChieu.split('T');
@@ -44,7 +45,7 @@ function DetailMoviePage(props) {
                             <TableCell>{new Date(ngayChieugioChieu).toLocaleTimeString()}</TableCell>
                             <TableCell>{item.thoiLuong} phút</TableCell>
                             <TableCell>{item.giaVe} VND</TableCell>
-                            <TableCell><Button>Mua vé</Button></TableCell>
+                            <TableCell><Button><Link to={`/boxoffice/${item.maLichChieu}`}>Mua vé</Link></Button></TableCell>
                         </TableRow>
                     })
                 })
