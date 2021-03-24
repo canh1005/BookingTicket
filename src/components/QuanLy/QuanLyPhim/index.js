24b4707b4d79169484fafbc98073a7faec1313e8
nguyenhuucanh1505@gmail.com <nguyenhuucanh1505@gmail.com>
2021-03-24 20:17:55 +0700

    Update QuanLyPhim

--- src/components/QuanLy/QuanLyPhim/index.js
+++ src/components/QuanLy/QuanLyPhim/index.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
-import Dashboard from '../../DashboardAdmin/Dashboard'
 import FormThemPhim from './FormThemPhim'
 
 export default class QuanLyPhim extends Component {
     render() {
         return (
             <div>
-                <Dashboard/>
                 <FormThemPhim/>
             </div>
         )
