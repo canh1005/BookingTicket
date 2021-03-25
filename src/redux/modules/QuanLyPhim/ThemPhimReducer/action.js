f9e9013fedfb538ef83ee5605e86e6853b8d9ba2
nguyenhuucanh1505@gmail.com <nguyenhuucanh1505@gmail.com>
2021-03-25 22:36:09 +0700

    Modified AddFilm

--- src/redux/modules/QuanLyPhim/ThemPhimReducer/action.js
+++ src/redux/modules/QuanLyPhim/ThemPhimReducer/action.js
@@ -1,34 +1,33 @@
 import api from "../../../../services/api";
-import { setHeader } from "../../../../utils/setHeaders";
 import * as Action from "./constances";
 
-export const actAddFilmAPI=(data, token)=>{
-    return dispatch=>{
+export const actAddFilmAPI = (data) => {
+    return dispatch => {
         dispatch(actAddFilmRequest());
-        api.post("/QuanLyPhim/ThemPhim",data, setHeader(token))
-        .then(rs=>{
-            dispatch(actAddFilmSuccess(rs.data))
-            alert("Thêm phim thành công !")
-        })
-        .catch(err=>{
-            dispatch(actAddFilmFailed(err))
-        })
+        console.log("DATA1", data);
+        api.post("/QuanLyPhim/ThemPhimUploadHinh", data)
+            .then(rs => {
+                dispatch(actAddFilmSuccess(rs.data))
+            })
+            .catch(err => {
+                dispatch(actAddFilmFailed(err))
+            })
     }
 }
 
-const actAddFilmRequest=()=>{
-    return{
+const actAddFilmRequest = () => {
+    return {
         type: Action.ADD_FILM_REQUEST
     }
 }
-const actAddFilmSuccess=(data)=>{
-    return{
+const actAddFilmSuccess = (data) => {
+    return {
         type: Action.ADD_FILM_SUCCESS,
         payload: data,
     }
 }
-const actAddFilmFailed=(err)=>{
-    return{
+const actAddFilmFailed = (err) => {
+    return {
         type: Action.ADD_FILM_FAILED,
         payload: err,
     }
