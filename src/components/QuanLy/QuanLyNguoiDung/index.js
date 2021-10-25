328d5ad9b4d6dc49fed96f2f41609f0791455930
CảnhNguyễn <nguyenhuucanh1505@gmail.com>
2021-10-25 17:37:16 +0700

    fix admin layout

--- src/components/QuanLy/QuanLyNguoiDung/index.js
+++ src/components/QuanLy/QuanLyNguoiDung/index.js
@@ -2,23 +2,19 @@ import React, { Component } from 'react';
 import { QuanLyTemplate } from '../../../styled/styled';
 import ListNguoiDung from '.././QuanLyNguoiDung/ListNguoiDung';
 import EditUserModal from '.././QuanLyNguoiDung/Modal/EditUserModal';
-// import DeleteUserModal from '.././QuanLyNguoiDung/Modal/DeleteUserModal';
 import SearchBar from '.././QuanLyNguoiDung/SearchBar';
 import ThemNguoiDung from '.././QuanLyNguoiDung/ThemNguoiDung';
 
 export default class QuanLyNguoiDung extends Component {
     render() {
-        console.log('aaa',this.props);
         return (
             <QuanLyTemplate>
                 <div className=''>
                     <div className=''>
                         <ThemNguoiDung />
                         <EditUserModal />
-                        {/* <DeleteUserModal /> */}
                     </div>
                     <div className="p-2">
-                        
                         <ListNguoiDung />
                     </div>
                 </div>
